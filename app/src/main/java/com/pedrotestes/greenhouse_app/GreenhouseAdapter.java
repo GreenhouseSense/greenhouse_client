b874c3945bce37f15c958a49505cc80dce2012bf
Jose Pedro Oliveira <joliveira@deec.uc.pt>
2018-10-26 00:16:59 +0100

    Added recycleView prototype on HomeScreen for list greenhouses from a user

---
+++ app/src/main/java/com/pedrotestes/greenhouse_app/GreenhouseAdapter.java
@@ -0,0 +1,73 @@
+package com.pedrotestes.greenhouse_app;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+class GreenhouseAdapter extends RecyclerView.Adapter<GreenhouseAdapter.ItemViewHolder> {
+
+
+    //this context we will use to inflate the layout
+    private Context mCtx;
+
+    //we are storing all the ptems in a list
+    private List<Greenhouse> ptemList;
+
+    //getting the context and ptem list with constructor
+    public GreenhouseAdapter(Context mCtx, List<Greenhouse> ptemList) {
+        this.mCtx = mCtx;
+        this.ptemList = ptemList;
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        //inflating and returning our view holder
+        LayoutInflater inflater = LayoutInflater.from(mCtx);
+        View view = inflater.inflate(R.layout.greenhouse_list_item, null);
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        //getting the ptem of the specified position
+        Greenhouse ptem = ptemList.get(position);
+
+        //binding the data with the viewholder views
+        holder.textViewTitle.setText(ptem.getTitle());
+        holder.textViewShortDesc.setText(ptem.getShortdesc());
+        holder.textViewRating.setText(String.valueOf(ptem.getRating()));
+        holder.textViewPrice.setText(String.valueOf(ptem.getPrice()));
+
+        holder.imageView.setImageDrawable(mCtx.getResources().getDrawable(ptem.getImage()));
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return ptemList.size();
+    }
+
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewTitle, textViewShortDesc, textViewRating, textViewPrice;
+        ImageView imageView;
+
+        public ItemViewHolder(View itemView) {
+            super(itemView);
+
+            textViewTitle = itemView.findViewById(R.id.textViewTitle);
+            textViewShortDesc = itemView.findViewById(R.id.textViewShortDesc);
+            textViewRating = itemView.findViewById(R.id.textViewRating);
+            textViewPrice = itemView.findViewById(R.id.textViewPrice);
+            imageView = itemView.findViewById(R.id.imageView);
+        }
+    }
+}
---
+++ app/src/main/java/com/pedrotestes/greenhouse_app/HomeActivity.java
@@ -0,0 +1,54 @@
+package com.pedrotestes.greenhouse_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HomeActivity extends AppCompatActivity {
+    //a list to store all the products
+    List<Greenhouse> greenhousesList;
+
+    //the recyclerview
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //initializing the productlist
+        greenhousesList = new ArrayList<>();
+
+
+        //adding some items to our list
+        for(int i = 0; i<10; i++) {
+            greenhousesList.add(
+                    new Greenhouse(
+                            i,
+                            "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                            "13.3 inch, Silver, 1.35 kg",
+                            4.3,
+                            60000,
+                            R.drawable.ic_android_black_24dp));
+        }
+
+
+        //creating recyclerview adapter
+        GreenhouseAdapter greenhouseAdapter = new GreenhouseAdapter(this, greenhousesList);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(greenhouseAdapter);
+
+
+    }
+}
